9dc4a69819ffdc576828a3728ebe7d2e1c85cb83
MihranGuyumjyan <mihran.guyumjyan@gmail.com>
2020-12-10 20:36:10 +0400

    timestamps

--- models/users.js
+++ models/users.js
@@ -1,11 +1,16 @@
 import { Schema, model } from "mongoose";
 
-const UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, unique: true },
-  password: { type: String, required: true },
-  age: { type: Number, required: false}
-});
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true },
+    password: { type: String, required: true },
+    age: { type: Number, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const User = model("User", UserSchema);
