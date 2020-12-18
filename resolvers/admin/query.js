41db7664b99eb6f3108ae72a9393aef7c76929cd
MihranGuyumjyan <mihran.guyumjyan@gmail.com>
2020-12-18 17:06:12 +0400

    changed admins query

--- resolvers/admin/query.js
+++ resolvers/admin/query.js
@@ -5,9 +5,9 @@ import { checkAuth } from '../../helpers/auth';
 export const adminQueries = {
   getAllUsers: async (root, {}, { userData }) => {
     checkAuth(userData);
-    if (!Number.isInteger(userData.userId))
-        throw new ApolloError("User dont have enough permissions");
-
-    return await User.find({}).exec();
+    if (Number.isInteger(userData.userId))
+      return await User.find({}).exec();
+    
+    throw new ApolloError("User dont have enough permissions");
   },
 };
